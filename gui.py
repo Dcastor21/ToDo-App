21ca601ea99d129b9548a2d4fea1539e9e408124
Darnel Castore <dcastor@student.clayton.edu>
2024-03-26 13:44:14 -0400

    Day 14

--- gui.py
+++ gui.py
@@ -5,6 +5,8 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="enter todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("My ToDo App", layout=[[label], [input_box],[add_button]])
+window = sg.Window("My ToDo App",
+                   layout=[[label], [input_box,add_button]],
+                   font= ('Helvetica',20))
 window.read()
 window.close()
