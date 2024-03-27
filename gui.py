5b43e38af08043ac10875979d3b778ed3af38851
Darnel Castore <dcastor@student.clayton.edu>
2024-03-27 16:51:37 -0400

    Day 19- implemented finishing touches

--- gui.py
+++ gui.py
@@ -2,12 +2,14 @@ import functions
 import time
 import PySimpleGUI as sg
 
+sg.theme("DarkTeal2")
+
 clock = sg.Text(' ', key ="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="enter todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
-                      enable_events=True, size=[45, 10])
+                      enable_events=True, size=[40,10])
 
 edit_button = sg.Button("edit")
 complete_button = sg.Button("Complete")
