61151b434424c092f8e07e3f47db593d814cde8a
Darnel Castore <dcastor@student.clayton.edu>
2024-03-28 13:13:16 -0400

    adding new_todo items feature

--- web.py
+++ web.py
@@ -17,7 +17,12 @@ st.write(" this app is here to increase prodcutivity and organize your tasks for
 st.text_input(label= " ", placeholder="Add New Task...",
               on_change=add_todo, key='new_todo')
 
-for todo in todos:
-    st.checkbox(todo)
+for index,  todo in enumerate(todos):
+   checkbox = st.checkbox(todo, key = todo)
+   if checkbox:
+       todos.pop(index)
+       functions.write_todos(todos)
+       del st.session_state[todo]
+       st.rerun()
 
 
